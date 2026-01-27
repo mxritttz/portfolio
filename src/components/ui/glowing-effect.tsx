be84d29d76fce71f5da40a8523a7008d4e1956b5
Renner Moritz <mr214@hdm-stuttgart.de>
2026-01-27 16:57:46 +0100

    added liquid glass to components fixed glow

--- src/components/ui/glowing-effect.tsx
+++ src/components/ui/glowing-effect.tsx
@@ -144,17 +144,17 @@ const GlowingEffect = memo(
                   var(--black),
                   var(--black) calc(25% / var(--repeating-conic-gradient-times))
                 )`
-                  : `radial-gradient(circle, #dd7bbb 10%, #dd7bbb00 20%),
-                radial-gradient(circle at 40% 40%, #d79f1e 5%, #d79f1e00 15%),
-                radial-gradient(circle at 60% 60%, #5a922c 10%, #5a922c00 20%), 
-                radial-gradient(circle at 40% 60%, #4c7894 10%, #4c789400 20%),
+                  : `radial-gradient(circle, #38bdf8 12%, #38bdf800 26%),
+                radial-gradient(circle at 40% 40%, #ec4899 10%, #ec489900 24%),
+                radial-gradient(circle at 60% 60%, #60a5fa 12%, #60a5fa00 26%), 
+                radial-gradient(circle at 40% 60%, #f472b6 12%, #f472b600 26%),
                 repeating-conic-gradient(
                   from 236.84deg at 50% 50%,
-                  #dd7bbb 0%,
-                  #d79f1e calc(25% / var(--repeating-conic-gradient-times)),
-                  #5a922c calc(50% / var(--repeating-conic-gradient-times)), 
-                  #4c7894 calc(75% / var(--repeating-conic-gradient-times)),
-                  #dd7bbb calc(100% / var(--repeating-conic-gradient-times))
+                  #38bdf8 0%,
+                  #ec4899 calc(25% / var(--repeating-conic-gradient-times)),
+                  #60a5fa calc(50% / var(--repeating-conic-gradient-times)), 
+                  #f472b6 calc(75% / var(--repeating-conic-gradient-times)),
+                  #38bdf8 calc(100% / var(--repeating-conic-gradient-times))
                 )`,
             } as React.CSSProperties
           }
